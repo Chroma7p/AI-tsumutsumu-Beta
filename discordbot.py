13c9b7caa54a05032870446b316d552556aa6b5b
Chroma7p <nahco3.hcl@gmail.com>
2023-03-04 15:24:12 +0900

    おベータ版ですわ

--- discordbot.py
+++ discordbot.py
@@ -3,65 +3,59 @@ from discord.ext import commands
 import discord
 import os
 import asyncio
+from dotenv import load_dotenv
+import openai
+from history import History
+
+
+load_dotenv()
 
 # デプロイ先の環境変数にトークンをおいてね
 APITOKEN = os.environ["DISCORD_BOT_TOKEN"]
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
 # botのオブジェクトを作成(コマンドのトリガーを!に)
-bot = commands.Bot(command_prefix="!",intents=discord.Intents.all())
-
-async def cog_setup(cogs):
-    for c in cogs:
-        await c.setup()
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 
-# コマンドを設定
-@bot.command()
-# "!hello"と送信された時
-async def hello(ctx):
-    await ctx.send("hello!")  # 送信された場所に"hello!"と送り返す
+channels = [985409309246644254, 1081461365694267453]
 
 
-# イベントを検知
+history = {channel: History(channel) for channel in channels}
+
+
+def completion(history):
+    return openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=history
+    )
+
+
 @bot.event
 # botの起動が完了したとき
 async def on_ready():
-    print("Hello!")  # コマンドラインにHello!と出力
+    for channelID in channels:
+        channel = bot.get_channel(channelID)
+        await channel.send("起動したよ！")
 
-
-# メッセージ編集時に発動(編集前(before)と後(after)のメッセージを送信)
-@bot.event
-async def on_message_edit(before, after):
-    txt = f"{before.author} がメッセージを編集しました！\nbefore:```{before.content}```\nafter:```{after.content}```"
-    await after.add_reaction()
-    await before.channel.send(txt)
-
-
-# メッセージ削除時に発動(削除されたメッセージを送信)
-@bot.event
-async def on_message_delete(message):
-    txt = f"{message.channel}:{message.author} がメッセージを削除しました！\n```{message.content}```"
-    await message.author.send(txt)
-    await message.channel.send(txt)
 
 @bot.event
-async def on_typing(channel,user,when):
-    txt=f"{user} がメッセージを入力しています！"
-    await channel.send(txt)
+async def on_message(message):
+    if not message.author.bot:
+        channelID = message.channel.id
+
+        print(channelID, message.content)
+        if channelID in channels:
+            history[channelID].history.append(
+                {"role": "user", "content": message.content})
+            result = completion(history[channelID].history)
+            history[channelID].history.append(result["choices"][0]["message"])
+            await message.channel.send(result["choices"][0]["message"]["content"])
 
 
 async def main():
-    # do other async things
-    # コグのフォルダ
-    cogfolder = "cogs."
-    # そして使用するコグの列挙(拡張子無しのファイル名)
-    cogs = ["sample_cog"]
-
-    for c in cogs:
-        await bot.load_extension(cogfolder + c)
-
     # start the client
     async with bot:
         await bot.start(APITOKEN)
 
 asyncio.run(main())
-
